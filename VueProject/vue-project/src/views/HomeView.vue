0434571903ac51fc55c801eb762a1518cb921a7c
dodido <idejrdud@gmail.com>
2022-11-19 00:56:20 +0900

    no message

--- VueProject/vue-project/src/views/HomeView.vue
+++ VueProject/vue-project/src/views/HomeView.vue
@@ -4,10 +4,18 @@
 
 <script>
 import HelloWorld from "../components/HelloWorld";
-
 export default {
   name: "HomeVue",
-
+  created() {
+    this.$axios
+      .get("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
   components: {
     HelloWorld,
   },
